1aeb646dd572baeb3a0a692a57053dab932891fa
Erika Ramírez <00157516@uca.edu.sv>
2018-04-06 14:51:58 -0600

    Envia foto y datos

--- app/src/main/java/com/ramirez/acercade/MainActivity.java
+++ app/src/main/java/com/ramirez/acercade/MainActivity.java
@@ -1,33 +1,69 @@
 package com.ramirez.acercade;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-     private Button compartir;
+    de.hdodenhof.circleimageview.CircleImageView idForSaveView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        idForSaveView=(de.hdodenhof.circleimageview.CircleImageView)findViewById(R.id.Erika);
+    }
 
-        compartir = findViewById(R.id.buttonShare);
+    public void OnClickShare(View view){
 
-        compartir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent sendIntent = new Intent ();
-                sendIntent.setAction (Intent.ACTION_SEND);
-                sendIntent.putExtra (Intent.EXTRA_STREAM,
-                        Uri.parse("android.resource://com.ramirez.acercade/drawable/" + Integer.toString(R.drawable.sii)));
-                sendIntent.setType ("image/*");
-                startActivity (sendIntent);
-            }
-        });
+        Bitmap bitmap =getBitmapFromView(idForSaveView);
+        try {
+            File file = new File(this.getExternalCacheDir(),"logicchip.png");
+            FileOutputStream fOut = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+            file.setReadable(true, false);
+            final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            intent.putExtra(Intent.EXTRA_TEXT, "Datos personales\nNombre: "+getResources().getString(R.string.nombre)+"\nCarrera: "+getResources().getString(R.string.carrera)
+                    +"\nGitHub: " + getResources().getString(R.string.git)+"\nTwitter: "+getResources().getString(R.string.twit)
+                    +"\nCorreo: "+ getResources().getString(R.string.correo)+"\nCelular: "+getResources().getString(R.string.cel));
+            intent.setType("*/*");
+            startActivity(Intent.createChooser(intent, "Enviar Datos en"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Bitmap getBitmapFromView(View view) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        Drawable bgDrawable =view.getBackground();
+        if (bgDrawable!=null) {
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas);
+        }   else{
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE);
+        }
+        view.draw(canvas);
+        return returnedBitmap;
     }
 }
